c99f59e2dc838b4c8ca6c8487d81047c4ea9a480
Sahil1551 <106240812+Sahil1551@users.noreply.github.com>
2024-06-27 23:33:14 +0530

    m54

--- src/components/PaymentVerification.jsx
+++ src/components/PaymentVerification.jsx
@@ -2,33 +2,11 @@ import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { CartContext } from './CartContext';
 import '../index.css';
-import jwt from 'jsonwebtoken'; 
 const PaymentVerification = () => {
   const { count, setCount } = useContext(CartContext);
   const [orderId, setOrderId] = useState(null); 
   const [CheckOut, setCheckout] = useState({});
 
-  useEffect(() => {
-    const getCookie = (name) => {
-      const value = `; ${document.cookie}`;
-      const parts = value.split(`; ${name}=`);
-      if (parts.length === 2) return parts.pop().split(';').shift();
-    };
-
-    const cookie = getCookie('data');
-    if (cookie) {
-      try {
-        // Decode JWT token to get payload
-        const decodedToken = jwt.decode(cookie);
-        console.log('Decoded JWT token:', decodedToken);
-        if (decodedToken) {
-          setOrderId(decodedToken.objectIdString);
-        }
-      } catch (error) {
-        console.error('Error decoding JWT:', error);
-      }
-    }
-  }, []);
   console.log('Parsed orderId:', orderId);
   useEffect(() => {
     const fetchCheckout = async () => {
